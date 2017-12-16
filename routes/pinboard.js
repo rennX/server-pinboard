b12f52669f287367040593d41ee4611d3bb8d725
Aaron Skinner <renn.xa@gmail.com>
2017-12-16 06:29:24 -0600

    implement /get and /dates

--- routes/pinboard.js
+++ routes/pinboard.js
@@ -6,16 +6,51 @@ const api_token = 'skinna123:15C82105FA77E7F20883';
 let pinboard = new Pinboard(api_token);
 let router = express.Router();
 
+const _logRequestData = function (req) {
+  console.log('body:', req.body);
+  console.log('params:', req.params);
+  console.log('query:', req.query);
+};
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a pinboard resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a pinboard resource');
+// });
+
+// TODO add method
+// TODO delete method
+
+router.get('/get', function(req, res, next) {
+  _logRequestData(req);
+  pinboard.get({}, function (err, response) {
+    if (err) res.send(err);
+    res.status(200).json(response);
+  });
 });
+
+router.get('/dates', function(req, res, next) {
+  _logRequestData(req);
+  pinboard.dates({}, function (err, response) {
+    if (err) res.send(err);
+    res.status(200).json(response);
+  });
+});
+
+// TODO recent method
 
 router.get('/all', function(req, res, next) {
   pinboard.all({}, function (err, response) {
-    res.send(response);
+    if (err) res.send(err);
+    res.status(200).json(response);
   });
 });
+
+// TODO suggest method
+// TODO getTags method
+// TODO delTag method
+// TODO renameTag method
+// TODO userSecret method
+// TODO api_token method
+// TODO listNotes method
+// TODO getNote method
 
 module.exports = router;
